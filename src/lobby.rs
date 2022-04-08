e1e13ae7524c4d4c02450e0b04175c239a4caa95
Berkay Dinç <berkayydinc@gmail.com>
2022-04-08 11:49:18 +0300

    Handle lobby events for the first client
    This is for testing purposes only, two of the clients will be handled in
    the future

--- src/lobby.rs
+++ src/lobby.rs
@@ -1,21 +1,48 @@
-use crate::client::Client;
+use crate::{
+    event::EventKind,
+    client::{ClientListenError, Client}
+};
+use futures_util::future::OptionFuture;
 use tokio::{
     select,
     sync::mpsc::{channel, Receiver, Sender},
 };
 
-pub struct Lobby;
+pub struct Lobby {
+    clients: (Option<Client>, Option<Client>),
+}
 
 impl Lobby {
+    fn new() -> Self {
+        Self { clients: (None, None) }
+    }
+
     async fn handle_lobby<F>(mut receiver: Receiver<Client>, on_client_release: F)
     where
         F: Fn(Client) + Send + 'static,
     {
+        let mut lobby = Self::new();
+
         loop {
+            let client0_listen_future: OptionFuture<_> = lobby.clients.0
+                .as_mut()
+                .map(|c| c.listen()).into();
+
             select! {
                 Some(client) = receiver.recv() => {
-                    on_client_release(client);
-                    break
+                    lobby.clients.0.replace(client);
+                },
+                Some(listen_result) = client0_listen_future => {
+                    match listen_result {
+                        Ok(event) => {
+                            match event.kind {
+                                EventKind::CloseConnection => break,
+                                _ => {}
+                            }
+                        },
+                        Err(ClientListenError::SocketStreamExhausted) => break,
+                        _ => {},
+                    }
                 },
                 else => {},
             }
