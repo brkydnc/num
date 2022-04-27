4dd88aa948b9f206cdc28787614ad58c59ef4a88
Berkay Dinç <berkayydinc@gmail.com>
2022-04-27 22:48:35 +0300

    Implement `fn emit()` for `Client`

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,8 @@
 use crate::event::{Event, EventKind};
-use futures_util::stream::StreamExt;
+use futures_util::{SinkExt, StreamExt};
 use tokio::net::TcpStream;
 use tokio_tungstenite::WebSocketStream;
-use tungstenite::Message;
+use tungstenite::{Message, Error as TungsteniteError };
 
 pub type WebSocket = WebSocketStream<TcpStream>;
 
@@ -38,5 +38,12 @@ impl Client {
             Message::Close(_) => Ok(Event::from(EventKind::CloseConnection)),
             _ => Err(ClientListenError::UnknownEvent),
         }
+    }
+
+    pub async fn emit(&mut self, event: &Event) -> Result<(), TungsteniteError> {
+        let json = serde_json::to_string(event)
+            .expect("Couldn't parse event into json string");
+
+        self.socket.send(Message::Text(json)).await
     }
 }
