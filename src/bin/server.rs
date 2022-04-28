6e1fe37e546cf71468b5e7c51553ea6b3bdf1dfa
Berkay Dinç <berkayydinc@gmail.com>
2022-04-28 15:08:11 +0300

    Introduce `Idler` to handle idling clients

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,9 +2,8 @@
 #![feature(entry_insert)]
 
 use bmrng::error::RequestError;
-use futures_util::future::{BoxFuture, FutureExt};
 use num::{
-    client::{Client, ClientListenError},
+    client::{Client, ClientListenError, ClientListenerState},
     event::EventKind,
     id::{Id, IdGenerator},
     lobby::{Lobby, Sender},
@@ -22,83 +21,109 @@ type LobbyIndex = Arc<RwLock<HashMap<Id, Sender>>>;
 static LOBBIES: SyncLazy<LobbyIndex> = SyncLazy::new(|| Arc::new(RwLock::new(HashMap::new())));
 static ID_GENERATOR: SyncLazy<IdGenerator> = SyncLazy::new(|| IdGenerator::new());
 
+struct Idler {
+    state: ClientListenerState
+}
+
+impl Idler {
+    fn new(client: Client) -> Self {
+        Self { state: ClientListenerState::Listen(client) }
+    }
+
+    async fn spawn(mut self) {
+        loop {
+            // Replace self.state with `Stop` temporarily.
+            if let ClientListenerState::Listen(mut client) = self.state.replace() {
+                match client.listen().await {
+                    Ok(event) => match event.kind {
+                        EventKind::CreateLobby => Self::on_create_lobby(client).await,
+
+                        EventKind::JoinLobby => Self::on_join_lobby(client, event.data).await,
+
+                        // self.state remains `Stop`, client gets dropped.
+                        EventKind::CloseConnection => {},
+
+                        // Continue listening only if the event is ignored.
+                        _ => { self.state.listen(client) }
+                    },
+
+                    // Cannot read the socket, self.state remains `Stop`,
+                    // so the client gets dropped.
+                    Err(ClientListenError::SocketStreamExhausted) => {},
+
+                    // Continue listening
+                    _ => { self.state.listen(client) }
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    async fn on_create_lobby(client: Client) {
+        let id = ID_GENERATOR.next();
+
+        let on_destroyed = move || {
+            LOBBIES
+                .try_write()
+                .expect("Error acquiring the lobby index lock")
+                .remove(&id);
+        };
+
+        let sender = Lobby::new(client)
+            .spawn(on_destroyed, handle_idle_client);
+
+        LOBBIES
+            .try_write()
+            .expect("Error acquiring the lobby index lock")
+            .entry(id)
+            .insert_entry(sender);
+    }
+
+    async fn on_join_lobby(mut client: Client, id_string: Option<String>) {
+        let id_parse = id_string
+            .map(|id| id.parse::<Id>().ok())
+            .flatten();
+
+        if let Some(id) = id_parse {
+            let client_sender = {
+                let lobbies = LOBBIES
+                    .try_read()
+                    .expect("Error acquiring the lobby index lock");
+
+                lobbies.get(&id).cloned()
+            };
+
+            if let Some(sender) = client_sender {
+                client = match sender.send_receive(client).await {
+                    Ok(response) => match response {
+                        Ok(_) => panic!(),
+                        Err(client) => client,
+                    },
+                    Err(request_error) => match request_error {
+                        RequestError::SendError(client) => client,
+                        _ => unreachable!(),
+                    },
+                };
+            }
+        }
+    }
+}
+
+fn handle_idle_client(client: Client) {
+    tokio::spawn(Idler::new(client).spawn());
+}
+
 async fn handle_new_connection(tcp_stream: TcpStream) {
     match tokio_tungstenite::accept_async(tcp_stream).await {
         Ok(socket) => {
             let client = Client::new(socket);
-            tokio::spawn(handle_idle_client(client));
+            handle_idle_client(client);
         },
         Err(cause) => {
             error!("Couldn't upgrade connection to websocket: {:?}", cause);
         }
     };
-}
-
-fn handle_idle_client(mut client: Client) -> BoxFuture<'static, ()> {
-    async move {
-        loop {
-            match client.listen().await {
-                Ok(event) => match event.kind {
-                    EventKind::CreateLobby => {
-                        let id = ID_GENERATOR.next();
-
-                        let on_destroyed = move || {
-                            LOBBIES
-                                .try_write()
-                                .expect("Error acquiring the lobby index lock")
-                                .remove(&id);
-                        };
-
-                        let on_client_release = |client| {
-                            tokio::spawn(handle_idle_client(client));
-                        };
-
-                        let sender = Lobby::new(client)
-                            .spawn(on_destroyed, on_client_release);
-
-                        LOBBIES
-                            .try_write()
-                            .expect("Error acquiring the lobby index lock")
-                            .entry(id)
-                            .insert_entry(sender);
-
-                        break;
-                    }
-                    EventKind::JoinLobby => {
-                        let parsed_id = event.data.map(|data| data.parse::<Id>().ok()).flatten();
-
-                        if let Some(id) = parsed_id {
-                            let client_sender = {
-                                let lobbies = LOBBIES
-                                    .try_read()
-                                    .expect("Error acquiring the lobby index lock");
-
-                                lobbies.get(&id).cloned()
-                            };
-
-                            if let Some(sender) = client_sender {
-                                client = match sender.send_receive(client).await {
-                                    Ok(response) => match response {
-                                        Ok(_) => break,
-                                        Err(client) => client,
-                                    },
-                                    Err(request_error) => match request_error {
-                                        RequestError::SendError(client) => client,
-                                        _ => unreachable!(),
-                                    },
-                                };
-                            }
-                        }
-                    }
-                    EventKind::CloseConnection => break,
-                    _ => {}
-                },
-                Err(ClientListenError::SocketStreamExhausted) => break,
-                _ => {}
-            }
-        }
-    }
-    .boxed()
 }
 
 const ADDRESS: &'static str = "127.0.0.1:7878";
