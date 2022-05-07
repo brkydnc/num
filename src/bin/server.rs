c9d59555089497ac8a739a8e015e3066c0e33baa
Berkay Dinç <berkayydinc@gmail.com>
2022-05-07 19:27:04 +0300

    Do not handle errors on websocket upgrade

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,21 +1,16 @@
 #![feature(once_cell)]
 #![feature(entry_insert)]
 
-use log::{error, info, debug};
+use log::{info, debug};
 use num::{client::Client, idler::Idler};
 use tokio::net::{TcpListener, TcpStream};
 
 async fn handle_new_connection(tcp_stream: TcpStream) {
-    match tokio_tungstenite::accept_async(tcp_stream).await {
-        Ok(socket) => {
-            let client = Client::new(socket);
-            Idler::spawn(client);
-            debug!("Connection upgraded to websocket");
-        }
-        Err(cause) => {
-            error!("Couldn't upgrade connection to websocket: {:?}", cause);
-        }
-    };
+    if let Ok(socket) = tokio_tungstenite::accept_async(tcp_stream).await {
+        let client = Client::new(socket);
+        Idler::spawn(client);
+        debug!("Connection upgraded to websocket");
+    }
 }
 
 const ADDRESS: &'static str = "127.0.0.1:7878";
