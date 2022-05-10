95825d147aaaa80f1eed733104e9e3540ae1fe4a
Berkay Dinç <berkayydinc@gmail.com>
2022-05-10 21:05:33 +0300

    Implement serde traits for `Secret`, invalidate `parse()`

--- src/secret.rs
+++ src/secret.rs
@@ -1,36 +1,65 @@
 use std::num::NonZeroU16;
+use std::fmt::{Formatter, Result as FormatResult};
+use serde::{
+    Serialize,
+    de::{Deserialize, Deserializer,Visitor, Error, Unexpected}
+};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Serialize)]
 pub struct Secret(NonZeroU16);
 
-impl Secret {
-    pub fn get(&self) -> u16 {
-        self.0.get()
+struct SecretVisitor;
+
+impl<'a> Visitor<'a> for SecretVisitor {
+    type Value = Secret;
+
+    fn expecting(&self, formatter: &mut Formatter) -> FormatResult {
+        formatter.write_str("a sequence of three unique digits")
     }
 
-    pub fn parse<T: AsRef<str>>(string: T) -> Option<Self> {
-        let number: u16 = string.as_ref().parse().ok()?;
+    fn visit_u64<E: Error>(self, n: u64) -> Result<Self::Value, E> {
+        if 988 > n && n > 10 {
+            let units = n % 10;
+            let tens = (n / 10) % 10;
+            let hundreds = (n / 100) % 10;
 
-        if number > 987 || number < 12 {
-            return None;
+            // The digits of the number must be unique.
+            if units != tens && units != hundreds && hundreds != tens {
+                unsafe {
+                    // SAFETY: The number is guaranteed to be less than u16::MAX.
+                    let n: u16 = n.try_into().unwrap_unchecked();
+
+                    // SAFETY: The number is guaranteed to be greater than zero in
+                    // this scope, because the first if conditon eliminated that
+                    // possibility earlier.
+                    Ok(Secret(NonZeroU16::new_unchecked(n)))
+                }
+            } else {
+                Err(Error::invalid_value(Unexpected::Unsigned(n), &self))
+            }
+        } else {
+            Err(Error::invalid_value(Unexpected::Unsigned(n), &self))
         }
+    }
+}
 
-        let units = number % 10;
-        let tens = (number / 10) % 10;
-        let hundreds = (number / 100) % 10;
+impl<'de> Deserialize<'de> for Secret {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_u16(SecretVisitor)
+    }
+}
 
-        // The digits of the number must be unique.
-        if !(units != tens && units != hundreds && hundreds != tens) {
-            return None;
-        }
-
-        // SAFETY: The number is guaranteed to be greater than zero above.
-        unsafe { Some(Secret(NonZeroU16::new_unchecked(number))) }
+impl Secret {
+    pub fn parse<T: AsRef<str>>(_: T) -> Option<Self> {
+        None
     }
 
     pub fn score(&self, guess: &Secret) -> (u8, u8) {
-        let secret = self.get();
-        let guess = guess.get();
+        let secret = self.0.get();
+        let guess = guess.0.get();
 
         let secret = [(secret / 100) % 10, (secret / 10) % 10, secret % 10];
         let guess = [(guess / 100) % 10, (guess / 10) % 10, guess % 10];
@@ -53,90 +82,57 @@ impl Secret {
 #[cfg(test)]
 mod test {
     use super::*;
+    use serde_json::{json, from_value};
 
-    #[test]
-    fn rejects_invalid_string() {
-        assert_eq!(Secret::parse("12a"), None);
-        assert_eq!(Secret::parse("as./,dkgfja.k/lsdjg"), None);
-        assert_eq!(Secret::parse("ab2"), None);
-        assert_eq!(Secret::parse(""), None);
-        assert_eq!(Secret::parse("879a"), None);
+    macro_rules! rejects {
+        ($($number:literal),*) => {
+            $(assert!(matches!(from_value::<Secret>(json!($number)), Err(_)));)*
+        }
     }
 
-    #[test]
-    fn rejects_invalid_u16() {
-        assert_eq!(Secret::parse("65536"), None);
-        assert_eq!(Secret::parse("-1"), None);
-        assert_eq!(Secret::parse("1293819023809123890"), None);
+    macro_rules! accepts {
+        ($($number:literal),*) => {
+            $(assert!(matches!(from_value::<Secret>(json!($number)), Ok(_)));)*
+        }
+    }
+
+    macro_rules! scores {
+        ($($secret:literal $guess:literal => $correct:literal $wrong:literal),*) => {
+            $(
+                let secret = from_value::<Secret>(json!($secret)).unwrap();
+                let guess = from_value::<Secret>(json!($guess)).unwrap();
+                assert_eq!(secret.score(&guess), ($correct, $wrong));
+             )*
+        }
     }
 
     #[test]
     fn rejects_invalid_range_of_numbers() {
-        assert_eq!(Secret::parse("1000"), None);
-        assert_eq!(Secret::parse("988"), None);
-        assert_eq!(Secret::parse("11"), None);
-        assert_eq!(Secret::parse("38252"), None);
-        assert_eq!(Secret::parse("3128"), None);
+        rejects!(0, 1000, 988, 11, 38252, 3128, 9238, 2381, -1, -1283, -123);
     }
 
     #[test]
     fn rejects_non_unique_numbers() {
-        assert_eq!(Secret::parse("22"), None);
-        assert_eq!(Secret::parse("101"), None);
-        assert_eq!(Secret::parse("199"), None);
-        assert_eq!(Secret::parse("911"), None);
-        assert_eq!(Secret::parse("666"), None);
-        assert_eq!(Secret::parse("383"), None);
-        assert_eq!(Secret::parse("311"), None);
-        assert_eq!(Secret::parse("339"), None);
-        assert_eq!(Secret::parse("155"), None);
-        assert_eq!(Secret::parse("112"), None);
-        assert_eq!(Secret::parse("99"), None);
+        rejects!(22, 101, 199, 911, 666, 383, 311, 339, 55, 112, 99, 133);
     }
 
     #[test]
-    fn parses_unique_three_digit_numbers() {
-        macro_rules! parse_assert_eq {
-            ($($secret:literal => $number:literal),*) => {
-                $(assert_eq!(Secret::parse($secret).unwrap().0.get(), $number);)*
-            }
-        }
-
-        parse_assert_eq! {
-            "123" => 123,
-            "152" => 152,
-            "921" => 921,
-            "756" => 756,
-            "987" => 987,
-            "012" => 12,
-            "019" => 19,
-            "536" => 536,
-            "671" => 671
-        }
+    fn accepts_unique_three_digit_numbers() {
+        accepts!(123, 152, 921, 756, 987, 12, 19, 536, 671);
     }
 
     #[test]
     fn scores_guesses() {
-        macro_rules! score_assert_eq {
-            ($($secret:literal $guess:literal => $correct:literal $wrong:literal),*) => {
-                $(
-                    assert_eq!(
-                        Secret::parse($secret).unwrap()
-                            .score(&Secret::parse($guess).unwrap()),
-                        ($correct, $wrong)
-                    )
-                ;)*
-            }
-        }
-
-        score_assert_eq! {
-            "123" "456" => 0 0,
-            "123" "123" => 3 0,
-            "123" "312" => 0 3,
-            "123" "321" => 1 2,
-            "123" "230" => 0 2,
-            "123" "923" => 2 0,
-            "123" "142" => 1 1
+        scores! {
+            123 456 => 0 0,
+            123 123 => 3 0,
+            123 312 => 0 3,
+            123 321 => 1 2,
+            123 230 => 0 2,
+            123 923 => 2 0,
+            123 142 => 1 1,
+            42 42 => 3 0
         }
     }
+
 }
