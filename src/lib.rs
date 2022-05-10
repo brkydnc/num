66ac6c54c70b0a7d6ad13b256ad93215b6758ba3
Berkay Dinç <berkayydinc@gmail.com>
2022-05-10 21:37:27 +0300

    Introduce `Directive` and `Notification` under `message` module

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,6 @@ pub mod game;
 pub mod idler;
 pub mod lobby;
 pub mod secret;
+pub mod message;
+
+pub use message::{Directive, Notification};
---
+++ src/message.rs
@@ -0,0 +1,29 @@
+use serde::{Serialize, Deserialize};
+use crate::{
+    lobby::Id,
+    secret::Secret,
+};
+
+#[non_exhaustive]
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+pub enum Directive {
+    CloseConnection,
+    CreateLobby,
+    JoinLobby { lobby_id: Id },
+    Leave,
+    SetSecret { secret: Secret },
+    StartGame,
+    Guess { secret: Secret },
+}
+
+#[non_exhaustive]
+#[derive(Debug, Serialize)]
+#[serde(tag = "type")]
+pub enum Notification<'a> {
+    // LobbyCreation { lobby_id: Id },
+    // LobbyJoin { lobby_id: Id },
+    SecretSet { secret: &'a Secret },
+    GuestJoin,
+    OpponentLeave,
+}
